63c6134def7b62d4149884d337daf1daddc42c61
gebhartn <nicholas.gebhart@gmail.com>
2020-08-22 12:49:21 -0700

    test: add user test

---
+++ src/user/user.spec.js
@@ -0,0 +1,43 @@
+// import { makeUser } from './'
+
+describe('user', () => {
+  it('must have an id', () => {
+    throw new Error('Not yet implemented')
+  })
+
+  it('must have an email', () => {
+    throw new Error('Not yet implemented')
+  })
+
+  it('must have a password', () => {
+    throw new Error('Not yet implemented')
+  })
+
+  it('must have a createdOn date', () => {
+    throw new Error('Not yet implemented')
+  })
+
+  it('must have an updatedOn date', () => {
+    throw new Error('Not yet implemented')
+  })
+
+  it('must have a hash', () => {
+    throw new Error('Not yet implemented')
+  })
+
+  it('can have a first name', () => {
+    throw new Error('Not yet implemented')
+  })
+
+  it('can have a last name', () => {
+    throw new Error('Not yet implemented')
+  })
+
+  it('validates the email', () => {
+    throw new Error('Not yet implemented')
+  })
+
+  it('validates the password', () => {
+    throw new Error('Not yet implemented')
+  })
+})
