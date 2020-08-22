c580e2c75040ea401b995443b29fabe89b178f3a
gebhartn <nicholas.gebhart@gmail.com>
2020-08-22 13:58:05 -0700

    refactor: remove generated fields

--- src/user/user.js
+++ src/user/user.js
@@ -1,24 +1,10 @@
 export function buildMakeUser({ md5, validate }) {
-  return function makeUser({
-    id,
-    email,
-    firstName,
-    lastName,
-    password,
-    updatedOn,
-    createdOn,
-  } = {}) {
+  return function makeUser({ email, firstName, lastName, password } = {}) {
     UserMustHave.prototype = new Error()
-
-    if (!id) throw new UserMustHave('an id.')
 
     if (!email) throw new UseCreationError('an email.')
 
     if (!password) throw new UserMustHave('a password.')
-
-    if (!updatedOn) throw new UserMustHave('an update date')
-
-    if (!createdOn) throw new UserMustHave('a creation date')
 
     if (!validate(email)) throw new UserMustHave('a valid email')
 
@@ -27,13 +13,10 @@ export function buildMakeUser({ md5, validate }) {
     let hash
 
     return Object.freeze({
-      getId: () => id,
       getEmail: () => email,
       getFirstName: () => firstName,
       getLastName: () => lastName,
       getPassword: () => password,
-      getUpdatedOn: () => updatedOn,
-      getCreatedOn: () => createdOn,
       getHash: () => hash || (hash = makeHash()),
     })
 
