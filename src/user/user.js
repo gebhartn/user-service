ba1d9955599d0ca3be8c4224f6182b0591b3870b
gebhartn <nicholas.gebhart@gmail.com>
2020-08-22 14:58:45 -0700

    fix: remove scuffed error

--- src/user/user.js
+++ src/user/user.js
@@ -1,14 +1,12 @@
 export function buildMakeUser({ md5, validate }) {
   return function makeUser({ email, firstName, lastName, password } = {}) {
-    UserMustHave.prototype = new Error()
+    if (!email) throw new Error('User must have an email.')
 
-    if (!email) throw new UseCreationError('an email.')
+    if (!password) throw new Error('User must have a password.')
 
-    if (!password) throw new UserMustHave('a password.')
+    if (!validate(email)) throw new Error('User must have a valid email')
 
-    if (!validate(email)) throw new UserMustHave('a valid email')
-
-    if (!validate(password)) throw new UserMustHave('a valid password')
+    if (!validate(password)) throw new Error('User must have a valid password')
 
     let hash
 
@@ -22,12 +20,6 @@ export function buildMakeUser({ md5, validate }) {
 
     function makeHash() {
       return md5(email + (firstName || '') + (lastName || '') + password)
-    }
-
-    function UserMustHave(message) {
-      this.name = 'UserMustHave'
-      this.message = 'User must have ' + message
-      this.stack = new Error().stack
     }
   }
 }
