32d579c5c83d7ed75c7b33ae974aa1e295aab5c1
gebhartn <nicholas.gebhart@gmail.com>
2020-08-22 14:01:34 -0700

    feat: add-user usecase

---
+++ src/use-cases/add-user.js
@@ -0,0 +1,21 @@
+import { makeUser } from '../user'
+
+export function makeAddUser({ usersDb }) {
+  return async function addUser(body) {
+    const user = makeUser(body)
+
+    const exists = await usersDb.findByHash({ user })
+
+    if (exists) return exists
+
+    return usersDb.insert({
+      user: {
+        email: user.getEmail(),
+        firstName: user.getFirstName(),
+        lastName: user.getLastName(),
+        password: user.getPassword(),
+        hash: user.getHash(),
+      },
+    })
+  }
+}
