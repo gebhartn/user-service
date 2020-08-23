09a96d43e8ba9ea1462532edd71c8ccbd8ef0db6
gebhartn <nicholas.gebhart@gmail.com>
2020-08-23 16:26:01 -0700

    feat: add edit-user route

--- src/routes/v1/users-router.js
+++ src/routes/v1/users-router.js
@@ -5,5 +5,6 @@ import { makeCallback } from '../../express-callback'
 export const users = Router()
 
 users.post('/', makeCallback(userController.postUser))
+users.patch('/:id', makeCallback(userController.patchUser))
 users.get('/', makeCallback(userController.getUsers))
 users.use(makeCallback(userController.notFound))
