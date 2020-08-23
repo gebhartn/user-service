d82ef6cde8d7602ef5f279242751c75d53631c55
gebhartn <nicholas.gebhart@gmail.com>
2020-08-23 12:28:25 -0700

    chore: add more sql scripts

---
+++ db/create.sql
@@ -0,0 +1,5 @@
+drop database if exists users;
+create database users;
+
+drop database if exists users_test;
+create database users_test;
---
+++ db/drop.sql
@@ -0,0 +1,2 @@
+drop database if exists users;
+drop database if exists users_test;
