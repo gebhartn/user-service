c041b4e6ff6649a31472eb4bad381855139ee048
gebhartn <nicholas.gebhart@gmail.com>
2020-08-23 14:49:38 -0700

    feat: add list-users

--- src/use-cases/list-users.js
+++ src/use-cases/list-users.js
@@ -1,7 +1,7 @@
 export function makeListUsers({ usersDb }) {
   return async function listUsers({ start = 0, count = 10 } = {}) {
     if (start < 0) start = 0
-    if (count < 10 || count < 1) count = 10
+    if (count > 10 || count < 1) count = 10
 
     const users = await usersDb.findAll({ start, count })
 
