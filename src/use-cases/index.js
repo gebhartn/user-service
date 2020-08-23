a7fff8ef6f441ebaa521eb5ef0a8df4abae67b2c
gebhartn <nicholas.gebhart@gmail.com>
2020-08-23 14:35:18 -0700

    feat: expose list-users

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -1,8 +1,11 @@
 import { makeAddUser } from './add-user'
+import { makeListUsers } from './list-users'
 import { usersDb } from '../data-access'
 
 export const addUser = makeAddUser({ usersDb })
+export const listUsers = makeListUsers({ usersDb })
 
 export const userService = Object.freeze({
   addUser,
+  listUsers,
 })
