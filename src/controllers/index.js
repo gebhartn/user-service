a8b6d36412e95e33b873b8c68532ffdb7724cf51
gebhartn <nicholas.gebhart@gmail.com>
2020-08-23 14:49:47 -0700

    feat: expose get-users

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,13 +1,15 @@
 import bcrypt from 'bcryptjs'
 
-import { addUser } from '../use-cases'
+import { addUser, listUsers } from '../use-cases'
 import { makePostUser } from './post-user'
+import { makeGetUsers } from './get-users'
 import { health } from './api-health'
 
 const hash = password => bcrypt.hashSync(password, 8)
 const validate = str => str.trim().length > 5
 
 export const postUser = makePostUser({ addUser, hash, validate })
+export const getUsers = makeGetUsers({ listUsers })
 
-export const userController = Object.freeze({ postUser })
+export const userController = Object.freeze({ postUser, getUsers })
 export const apiController = Object.freeze({ health })
