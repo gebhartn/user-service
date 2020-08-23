ccc8b31349447b79a8c44b2ee067c95429f703d7
gebhartn <nicholas.gebhart@gmail.com>
2020-08-23 16:25:44 -0700

    feat: patch users

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,8 @@
 import bcrypt from 'bcryptjs'
 
-import { addUser, listUsers } from '../use-cases'
+import { addUser, listUsers, editUser } from '../use-cases'
 import { makePostUser } from './post-user'
+import { makePatchUser } from './patch-user'
 import { makeGetUsers } from './get-users'
 import { health } from './api-health'
 import { notFound as not } from './not-found'
@@ -10,8 +11,15 @@ const hash = password => bcrypt.hashSync(password, 8)
 const validate = str => str.trim().length > 5
 
 export const postUser = makePostUser({ addUser, hash, validate })
+export const patchUser = makePatchUser({ editUser })
 export const getUsers = makeGetUsers({ listUsers })
 export const notFound = not
 
-export const userController = Object.freeze({ postUser, getUsers, notFound })
+export const userController = Object.freeze({
+  postUser,
+  patchUser,
+  getUsers,
+  notFound,
+})
+
 export const apiController = Object.freeze({ health })
---
+++ src/controllers/patch-user.js
@@ -0,0 +1,41 @@
+export function makePatchUser({ editUser }) {
+  return async function patchUser(request) {
+    try {
+      const { ...user } = request.body
+
+      const updating = {
+        ...user,
+        id: request.params.id,
+      }
+
+      const patched = await editUser(updating)
+
+      return {
+        headers: {
+          'Content-Type': 'application/json',
+          'Last-Modified': new Date().toUTCString(),
+        },
+        status: 200,
+        body: { patched },
+      }
+    } catch (e) {
+      console.log(e)
+      if (e.name === 'RangeError') {
+        return {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          status: 404,
+          body: { error: e.message },
+        }
+      }
+      return {
+        headers: {
+          'Content-Type': 'applcation/json',
+        },
+        status: 400,
+        body: { error: e.message },
+      }
+    }
+  }
+}
