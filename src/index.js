d4c1e3f974d2136ed390cca751aa7ffdd60aeb5b
gebhartn <nicholas.gebhart@gmail.com>
2020-08-22 22:54:59 -0700

    chore: exit on errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+import http from 'http'
+
 import { makeApp } from './app'
 import { middleware } from './middleware'
 import { routes } from './routes'
 
+process.on('uncaughtException', e => {
+  console.error(e)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', e => {
+  console.error(e)
+  process.exit(1)
+})
+
 const port = process.env.PORT || 8080
 const app = makeApp({ middleware, routes })
+const server = http.createServer(app)
 
-app.listen(port, () => console.log('Listening on port: ' + port))
+server.listen(port, () => console.log('Listening on port: ' + port))
