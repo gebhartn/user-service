a9544721339134ac33c9f82ff10fb5db2f166e8f
gebhartn <nicholas.gebhart@gmail.com>
2020-08-23 14:50:31 -0700

    chore: database name

--- src/data-access/index.js
+++ src/data-access/index.js
@@ -6,6 +6,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 })
 
 export function makeDb() {
