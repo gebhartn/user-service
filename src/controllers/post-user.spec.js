b008cbfae2c318b6d1b3d9af5b9281d0f3e47024
gebhartn <nicholas.gebhart@gmail.com>
2020-08-22 17:05:36 -0700

    test: post-user test

---
+++ src/controllers/post-user.spec.js
@@ -0,0 +1,12 @@
+import { makePostUser } from './post-user'
+import { makeFakeUser } from '../../__test__/fixtures/user'
+
+describe('post user controller', () => {
+  it('successfully posts a user', async () => {
+    throw new Error('Not yet implemented', { makePostUser, makeFakeUser })
+  })
+
+  it('reports user errors', async () => {
+    throw new Error('Not yet implemented', { makePostUser, makeFakeUser })
+  })
+})
