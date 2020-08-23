6126d1177cf3f7587110f81966b1759f2325d8bf
gebhartn <nicholas.gebhart@gmail.com>
2020-08-23 16:26:27 -0700

    refactor: edit make-user logic

--- src/user/user.js
+++ src/user/user.js
@@ -1,9 +1,5 @@
 export function buildMakeUser({ md5 }) {
   return function makeUser({ email, firstName, lastName, password } = {}) {
-    if (!email) throw new Error('User must have an email.')
-
-    if (!password) throw new Error('User must have a password.')
-
     let hash
 
     return Object.freeze({
