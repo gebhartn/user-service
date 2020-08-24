51d0c0c9fdf8627e218580c1fa6f1efb6a3c2905
gebhartn <nicholas.gebhart@gmail.com>
2020-08-24 10:39:16 -0700

    test: connection string

--- __tests__/fixtures/db.js
+++ __tests__/fixtures/db.js
@@ -1,23 +1,6 @@
 import { Pool } from 'pg'
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_TEST,
-// })
-
-const config =
-  process.env.NODE_ENV === production
-    ? { connectionString: process.env.DB_HEOKU }
-    : {
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_TEST,
-      }
+const config = { connectionString: process.env.DB_HEOKU }
 
 const pool = new Pool({ ...config })
 
