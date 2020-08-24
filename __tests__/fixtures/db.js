94dba08ef2c663997e544be1ae497d0b90d09707
gebhartn <nicholas.gebhart@gmail.com>
2020-08-23 19:25:38 -0700

    test: mock db

---
+++ __tests__/fixtures/db.js
@@ -0,0 +1,17 @@
+import { Pool } from 'pg'
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_TEST,
+})
+
+export function makeDb() {
+  return Object.freeze({ query })
+
+  async function query({ query }) {
+    return pool.query(query)
+  }
+}
