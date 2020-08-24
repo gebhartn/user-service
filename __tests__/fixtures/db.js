af9b5e34d947799a6fa724c42f954db08f604201
gebhartn <nicholas.gebhart@gmail.com>
2020-08-24 10:36:04 -0700

    test: trying out test db

--- __tests__/fixtures/db.js
+++ __tests__/fixtures/db.js
@@ -1,12 +1,25 @@
 import { Pool } from 'pg'
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_TEST,
-})
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_TEST,
+// })
+
+const config =
+  process.env.NODE_ENV === production
+    ? { connectionString: process.env.DB_HEOKU }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_TEST,
+      }
+
+const pool = new Pool({ ...config })
 
 export function makeDb() {
   return Object.freeze({ query })
