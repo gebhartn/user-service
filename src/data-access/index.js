77441b4b62e8502885c3b0fd8d09a302341a10f4
gebhartn <nicholas.gebhart@gmail.com>
2020-08-24 11:13:31 -0700

    test: deploy

--- src/data-access/index.js
+++ src/data-access/index.js
@@ -2,11 +2,7 @@ import { makeUsersDb } from './users-db'
 import { Pool } from 'pg'
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
 })
 
 export function makeDb() {
