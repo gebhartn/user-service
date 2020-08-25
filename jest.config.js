2933a64d34e0e4df2e809cf09ce1d063e3d11f3e
gebhartn <nicholas.gebhart@gmail.com>
2020-08-25 10:23:34 -0700

    test: ignore myself

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   verbose: true,
   collectCoverageFrom: [
     '**/*.{js,jsx}',
+    '!**/*.*.{js,jsx}',
     '!**/node_modules/**',
     '!**/vendor/**',
     '!**/__tests__/**',
