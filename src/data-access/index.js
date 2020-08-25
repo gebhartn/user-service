bde34c192a858456a6c307a5682a67bdffe1777c
gebhartn <nicholas.gebhart@gmail.com>
2020-08-24 17:13:20 -0700

    chore: deploy

--- src/data-access/index.js
+++ src/data-access/index.js
@@ -2,7 +2,10 @@ import { makeUsersDb } from './users-db'
 import { Pool } from 'pg'
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    process.env.NODE_ENV === 'production'
+      ? process.env.DATABASE_URL
+      : process.env.DATABASE_LOCAL,
 })
 
 export function makeDb() {
