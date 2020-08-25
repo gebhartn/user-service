c88fe7c47c2b0422485d93759d038efcaa06b591
gebhartn <nicholas.gebhart@gmail.com>
2020-08-24 17:26:09 -0700

    test: ssl please

--- __tests__/fixtures/db.js
+++ __tests__/fixtures/db.js
@@ -1,6 +1,9 @@
 import { Pool } from 'pg'
 
-const config = { connectionString: process.env.DATABASE_URL }
+const config = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+}
 
 const pool = new Pool({ ...config })
 
