ba7303d4bd8c83fa3d79c5a59dd16806c71300e3
gebhartn <nicholas.gebhart@gmail.com>
2020-08-24 21:37:06 -0700

    chore: bigger seed

--- db/seed.sql
+++ db/seed.sql
@@ -2,3 +2,4 @@ insert into users(email, first_name, last_name, password, hash_code) values('jua
 insert into users(email, first_name, last_name, password, hash_code) values('nicholas@gmail.com', 'nicholas', 'sucks', 'weakpass1', 'fake_hashcode2') returning id;
 insert into users(email, first_name, last_name, password, hash_code) values('pogchamp@gmail.com', 'pog', 'champ', 'impossiblerubypuzzlekingom', 'fake_hashcode3') returning id;
 insert into users(email, first_name, last_name, password, hash_code) values('hikarunakamura@gmail.com', 'hikaru', 'nakamura', 'overwatchcreationistdoodlerfae', 'fake_hashcode4') returning id;
+insert into users(email, first_name, last_name, password, hash_code) values('tony@gmail.com', 'tony', 'k', 'collectgraphqlbrainpower', 'fake_hashcode5') returning id;
